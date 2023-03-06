eabb5387d6cffd869a32d6ef0b6db6dc978f257d
Patrick Flores <savvyscheme@gmail.com>
2023-03-06 02:57:57 -0700

    Added listener for when project is created

--- resources/views/livewire/select-project.blade.php
+++ resources/views/livewire/select-project.blade.php
@@ -1,7 +1,11 @@
-<select name="projects" id="projects" class="w-full p-2 mb-4">
-    <option value="" disabled selected>SELECT A PROJECT</option>
-    @foreach($projects as $project)
-        <option value="{{ $project->id }}">{{$project->name}}</option>
-    @endforeach
-    <!-- <option value="chat-gpt-app">ChatGPT App</option> -->
-</select>
+
+<div class="pb-2 mb-4 flex">
+    <select wire:model="project_id" name="projects" id="projects" class="w-full">
+        <option value="" disabled selected>SELECT A PROJECT</option>
+        @foreach($projects as $project)
+            <option value="{{ $project->id }}">{{$project->name}}</option>
+        @endforeach
+        <!-- <option value="chat-gpt-app">ChatGPT App</option> -->
+    </select>
+    <button wire:click="delete" class="px-4 py-2 border-b border-gray-300 bg-red-500 shadow-md text-white">DELETE</button>
+</div>
