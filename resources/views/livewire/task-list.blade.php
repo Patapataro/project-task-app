39e234174da104e8bea9c908e692a127e9c103d0
Patrick Flores <savvyscheme@gmail.com>
2023-03-06 03:31:55 -0700

    Added for loop for each task

--- resources/views/livewire/task-list.blade.php
+++ resources/views/livewire/task-list.blade.php
@@ -1,3 +1,6 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-</div>
+<ul class=" mx-auto">
+    @foreach($tasks as $task)
+        <li class="p-2 border flex justify-between border-gray-300"><span>{{ $task->name }}</span><button class="px-4 text-red-500 font-bold font-sans">X</button></li>
+    @endforeach
+    <!-- <li class="p-2 border flex justify-between border-gray-300"><span>Task Two</span><button class="px-4 text-red-500 font-bold font-sans">X</button></li> -->
+</ul>
