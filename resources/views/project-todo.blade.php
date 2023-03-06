2ccfdf1fb637f673d205ea6ec2618faba0d6e580
Patrick Flores <savvyscheme@gmail.com>
2023-03-06 03:39:58 -0700

    Added add-task component to main app

--- resources/views/project-todo.blade.php
+++ resources/views/project-todo.blade.php
@@ -9,12 +9,8 @@
         <!-- View and edit tasks section -->
         <div class="p-6 border-b border-gray-300">
             <h2 class="text-xl mb-4">Tasks</h2>
-            <div class="flex pb-4 mx-4">
-                <div class="border-b border-gray-300 inline-block w-full">
-                    <input type="text" placeholder="New Task" class="w-full border-none">
-                </div>
-                <button class="px-4 py-2 border-b border-gray-300 bg-blue-500 shadow-md text-white">ADD</button>
-            </div>
+
+            @livewire('add-task')
 
             @livewire('select-project')
 
