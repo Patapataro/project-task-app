bfd30dcc3339c0b5f7ff8b224b704aa716dcf302
Patrick Flores <savvyscheme@gmail.com>
2023-03-06 03:31:09 -0700

    Added event listenter for project change to task-list component

--- app/Http/Livewire/TaskList.php
+++ app/Http/Livewire/TaskList.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Project;
 
 class TaskList extends Component
 {
+    public $tasks = [];
+
+    protected $listeners = ['updateProject' => 'mount'];
+
+
+    public function mount($project_id = null)
+    {
+        if($project_id != null) {
+            $this->tasks = Project::find($project_id)->tasks;
+        }
+    }
+
     public function render()
     {
         return view('livewire.task-list');
