65cd12d1b4c1c28b5a8a3792e5fdb452fb285be6
Patrick Flores <savvyscheme@gmail.com>
2023-03-06 22:04:55 -0700

    Added addTask method

--- app/Http/Livewire/AddTask.php
+++ app/Http/Livewire/AddTask.php
@@ -3,9 +3,25 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Project;
 
 class AddTask extends Component
 {
+    public $name;
+    public $project_id;
+
+    public function addTask()
+    {
+        $project = Project::find($this->project_id);
+        $max = $project->tasks->max('priority');
+        $task = $project->tasks->create([
+            'project_id' => $this->project_id,
+            'priority' => $max + 1,
+            'name' => $this->name,
+            'completed' => null,
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.add-task');
--- resources/views/livewire/add-task.blade.php
+++ resources/views/livewire/add-task.blade.php
@@ -1,3 +1,6 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class="flex pb-4 mx-4">
+    <div class="border-b border-gray-300 inline-block w-full">
+        <input wire:bind="name" type="text" placeholder="New Task" class="w-full border-none">
+    </div>
+    <button wire:click="addTask" class="px-4 py-2 border-b border-gray-300 bg-blue-500 shadow-md text-white">ADD</button>
 </div>
