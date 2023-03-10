6cbd008f6304b753af9437483b7c7094ec27843b
Patrick Flores <savvyscheme@gmail.com>
2023-03-10 15:01:24 -0700

    Updated select padding

--- resources/views/livewire/select-project.blade.php
+++ resources/views/livewire/select-project.blade.php
@@ -1,6 +1,6 @@
 
 <div class="pb-2 mb-4 flex">
-    <select wire:model="project_id" name="projects" data-cy="projects" class="w-full">
+    <select wire:model="project_id" name="projects" data-cy="projects" class="pl-2 w-full">
         <option value="" selected>SELECT A PROJECT</option>
         @foreach($projects as $project)
             <option value="{{ $project->id }}">{{$project->name}}</option>
