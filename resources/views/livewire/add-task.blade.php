c047fa4277512e02abb607a8af5ff0ae56fb1715
Patrick Flores <savvyscheme@gmail.com>
2023-03-10 15:00:39 -0700

    Added on keydown function to inputs

--- resources/views/livewire/add-task.blade.php
+++ resources/views/livewire/add-task.blade.php
@@ -1,6 +1,6 @@
 <div class="flex pb-4 mx-4">
     <div class="border-b border-gray-300 inline-block w-full">
-        <input wire:model="name" data-cy="new-task-input" type="text" placeholder="New Task" class="w-full border-none">
+        <input wire:keydown.enter="addTask" wire:model="name" data-cy="new-task-input" type="text" placeholder="New Task" class="w-full border-none">
     </div>
     <button wire:click="addTask" data-cy="new-task-submit" class="px-4 py-2 border-b border-gray-300 bg-blue-500 shadow-md text-white">ADD</button>
 </div>
--- resources/views/livewire/create-project.blade.php
+++ resources/views/livewire/create-project.blade.php
@@ -3,7 +3,7 @@
     <!-- @error('name') <span class="error mx-4 text-red-600">{{ $message }}</span> @enderror -->
     <div class="flex mx-4">
         <div class="border-b border-gray-300 inline-block w-full">
-            <input wire:model="name" data-cy="new-project-input" name="project-name" type="text" placeholder="New Project" class="w-full border-none">
+            <input wire:keydown.enter="submit" wire:model="name" data-cy="new-project-input" name="project-name" type="text" placeholder="New Project" class="w-full border-none">
         </div>
 
         <button wire:click="submit" data-cy="new-project-submit" class="px-4 py-2 border-b border-gray-300 bg-green-500 shadow-md text-white">CREATE</button>
